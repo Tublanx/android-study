6f9f0688b0292e2fbf50c4efc5a9f09a7dcd673e
Tublanx <rlgus03453@naver.com>
2022-12-16 22:34:42 +0900

    loading demo

--- LoadingDemo/app/src/main/java/com/example/loadingdemo/MainActivity.java
+++ LoadingDemo/app/src/main/java/com/example/loadingdemo/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.loadingdemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ProgressBar progressBar = findViewById(R.id.progress_charging);
+
+        new Thread(() -> {
+            try {
+                int i = 1;
+                while (i <= 500) {
+                    i += 50;
+                    progressBar.setProgress(i);
+                    Thread.sleep(500);
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
+
     }
 }
