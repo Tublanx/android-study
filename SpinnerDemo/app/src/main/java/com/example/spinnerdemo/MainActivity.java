8684fc6b6aa8d4a7835bfe62346afc5ba8cddeb3
Tublanx <rlgus03453@naver.com>
2022-12-16 21:48:39 +0900

    init components & add spinner event

--- SpinnerDemo/app/src/main/java/com/example/spinnerdemo/MainActivity.java
+++ SpinnerDemo/app/src/main/java/com/example/spinnerdemo/MainActivity.java
@@ -3,12 +3,36 @@ package com.example.spinnerdemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Spinner spinner;
+    private TextView txtResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // initialize components
+        spinner = findViewById(R.id.activity_main_spinner);
+        txtResult = findViewById(R.id.activity_main_txt_result);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                txtResult.setText(adapterView.getItemAtPosition(i).toString());
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
     }
 }
