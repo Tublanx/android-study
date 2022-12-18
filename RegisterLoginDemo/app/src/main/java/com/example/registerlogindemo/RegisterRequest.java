33304e33ac5def59fc9dd6859f547e512467198f
Tublanx <rlgus03453@naver.com>
2022-12-18 15:36:59 +0900

    register server url setting

--- RegisterLoginDemo/app/src/main/java/com/example/registerlogindemo/RegisterRequest.java
+++ RegisterLoginDemo/app/src/main/java/com/example/registerlogindemo/RegisterRequest.java
@@ -1,2 +1,35 @@
-package com.example.registerlogindemo;public class RegisterRequest {
+package com.example.registerlogindemo;
+
+import androidx.annotation.Nullable;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RegisterRequest extends StringRequest {
+
+    // 서버 URL 설정 (php 파일 연동)
+    private final static String URL = "http://tublanx.ivyro.net/Register.php";
+    private Map<String, String> map;
+
+    public RegisterRequest(String userID, String userPwd, String userName, int userAge, Response.Listener<String> listener) {
+
+        super(Method.POST, URL, listener, null);
+
+        map = new HashMap<>();
+        map.put("userID", userID);
+        map.put("userPwd", userPwd);
+        map.put("userName", userName);
+        map.put("userAge", userAge + "");
+
+    }
+
+    @Nullable
+    @Override
+    protected Map<String, String> getParams() throws AuthFailureError {
+        return map;
+    }
 }
