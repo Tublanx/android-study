d9b1cea953928c7c94eae857f44520d9a7a8f0f7
Tublanx <rlgus03453@naver.com>
2022-12-18 16:12:43 +0900

    main activity setting

--- RegisterLoginDemo/app/src/main/java/com/example/registerlogindemo/MainActivity.java
+++ RegisterLoginDemo/app/src/main/java/com/example/registerlogindemo/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.registerlogindemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView tvId, tvPwd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvId = findViewById(R.id.activity_main_tv_id);
+        tvPwd = findViewById(R.id.activity_main_tv_pwd);
+
+        Intent intent = getIntent();
+        String userId = intent.getStringExtra("userId");
+        String userPwd = intent.getStringExtra("userPwd");
+
+        tvId.setText(userId);
+        tvPwd.setText(userPwd);
+
     }
 }
