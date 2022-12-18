fdddef4e87d8078eed6da32355cb429e0f9c0d54
Tublanx <rlgus03453@naver.com>
2022-12-18 16:12:50 +0900

    register setting

--- RegisterLoginDemo/app/src/main/java/com/example/registerlogindemo/RegisterActivity.java
+++ RegisterLoginDemo/app/src/main/java/com/example/registerlogindemo/RegisterActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -52,6 +53,7 @@ public class RegisterActivity extends AppCompatActivity {
 
                             // 회원가입 성공
                             if (isSuccess) {
+                                Log.e("userPassword : ", userPwd);
                                 Toast.makeText(getApplicationContext(), "회원 등록에 성공하였습니다.", Toast.LENGTH_SHORT).show();
                                 Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
                                 startActivity(intent);
