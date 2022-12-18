1e1555a2513dbe146bba578eface3a7a690c4a73
Tublanx <rlgus03453@naver.com>
2022-12-18 15:36:49 +0900

    Login server url setting

--- RegisterLoginDemo/app/src/main/java/com/example/registerlogindemo/LoginRequest.java
+++ RegisterLoginDemo/app/src/main/java/com/example/registerlogindemo/LoginRequest.java
@@ -9,21 +9,19 @@ import com.android.volley.toolbox.StringRequest;
 import java.util.HashMap;
 import java.util.Map;
 
-public class RegisterRequest extends StringRequest {
+public class LoginRequest extends StringRequest {
 
     // 서버 URL 설정 (php 파일 연동)
-    private final static String URL = "http://tublanx.ivyro.net/Register.php";
+    private final static String URL = "http://tublanx.ivyro.net/Login.php";
     private Map<String, String> map;
 
-    public RegisterRequest(String userID, String userPwd, String userName, int userAge, Response.Listener<String> listener) {
+    public LoginRequest(String userID, String userPwd, Response.Listener<String> listener) {
 
         super(Method.POST, URL, listener, null);
 
         map = new HashMap<>();
         map.put("userID", userID);
         map.put("userPwd", userPwd);
-        map.put("userName", userName);
-        map.put("userAge", userAge + "");
 
     }
 
